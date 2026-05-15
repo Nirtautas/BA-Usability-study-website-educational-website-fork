bd6d54c00830ca171e601bcc7722aa2e30275b06
Nirtautas Šadauskas <nirtautas.sadauskas@gmail.com>
2026-05-15 15:11:19 +0300

    exercise fix

--- src/components/shared/keepsBoxServiceDialog.tsx
+++ src/components/shared/keepsBoxServiceDialog.tsx
@@ -7,8 +7,8 @@ const KeepsBoxServiceDialogInfo = () => {
   return (
     <Stack direction="column" alignItems="center" gap={1}>
       <Typography>{t("Description.mainExplanation")}</Typography>
-      <Typography exercise-step="hardToCancelSelected">{t("Description.paymentExplanation")}</Typography>
-      <Typography>{t("Description.cancellationExplanation")}</Typography>
+      <Typography>{t("Description.paymentExplanation")}</Typography>
+      <Typography exercise-step="hardToCancelSelected">{t("Description.cancellationExplanation")}</Typography>
     </Stack>
   );
 };
